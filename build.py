1a406e9ebfe8e10ffa7795792055b5a8bba90d0a
rp.wang <694287962@qq.com>
2026-04-10 14:59:59 +0800

    删除中间脚本

--- build.py
+++
@@ -1,6 +0,0 @@
-import os  
-def wf(p, t):  
-    os.makedirs(os.path.dirname(p), exist_ok=True)  
-    with open(p, 'w', encoding='utf-8') as f: f.write(t)  
-    print(f'Written: {len(t)} chars')  
-print('build.py ready')  
--- write_files.py
+++
@@ -1,8 +0,0 @@
-﻿import os
-
-def cf(p, c):
-    os.makedirs(os.path.dirname(p), exist_ok=True)
-    with open(p, chr(119), encoding=chr(117)+chr(116)+chr(102)+chr(45)+chr(56)) as fh:
-        fh.write(c)
-    print(chr(67)+chr(114)+chr(101)+chr(97)+chr(116)+chr(101)+chr(100)+chr(58)+chr(32)+p)
-print(chr(114)+chr(101)+chr(97)+chr(100)+chr(121))
